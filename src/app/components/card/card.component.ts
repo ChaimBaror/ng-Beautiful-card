5ff12070d4ab6c2075832ca1338712ac5a655e47
myusername <myemail>
2020-11-03 19:34:27 +0200

    first commit

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -66,33 +66,4 @@ export class CardComponent implements AfterViewInit {
     this.renderer.setStyle(this.purchase.nativeElement, 'transform', `translateZ(0px)`)
   }
 
-
-  // container.addEventListener("mousemove", (e) => {
-  //   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-  //   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-  //   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-  // });
-  // //Animate In
-  // container.addEventListener("mouseenter", (e) => {
-  //   card.style.transition = "none";
-  //   //Popout
-  //   title.style.transform = "translateZ(150px)";
-  //   sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-  //   description.style.transform = "translateZ(125px)";
-  //   sizes.style.transform = "translateZ(100px)";
-  //   purchase.style.transform = "translateZ(75px)";
-  // });
-  // //Animate Out
-  // container.addEventListener("mouseleave", (e) => {
-  //   card.style.transition = "all 0.5s ease";
-  //   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  //   //Popback
-  //   title.style.transform = "translateZ(0px)";
-  //   sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-  //   description.style.transform = "translateZ(0px)";
-  //   sizes.style.transform = "translateZ(0px)";
-  //   purchase.style.transform = "translateZ(0px)";
-  // });
-
-
 }
